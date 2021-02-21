26caf04b96448ab829ba26a1a5a1cd393343fa4e
Elijah V. Schrotenboer <fidgetybog@gmail.com>
2021-02-21 08:43:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,69 @@
 import gym
 import gym_game
 
-import os
-
-import numpy as np
-
 from stable_baselines.common.policies import CnnLstmPolicy
 from stable_baselines import PPO2
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines.common.callbacks import BaseCallback
-from stable_baselines.results_plotter import load_results, ts2xy
+from stable_baselines.bench import Monitor
 
 
-
+log_dir = "D:/Hmm/logs"
 env_id = "BattleFront2-v0"
 
+
+class SaveOnBestTrainingRewardCallback(BaseCallback):
+    """
+    Callback for saving a model (the check is done every ``check_freq`` steps)
+    based on the training reward (in practice, we recommend using ``EvalCallback``).
+    :param check_freq: (int)
+    :param log_dir: (str) Path to the folder where the model will be saved.
+      It must contains the file created by the ``Monitor`` wrapper.
+    :param verbose: (int)
+    """
+    def __init__(self, check_freq: int, log_dir: str, verbose=1):
+        super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.log_dir = log_dir
+        self.save_path = os.path.join(log_dir, 'best_model')
+        self.best_mean_reward = -np.inf
+
+    def _init_callback(self) -> None:
+        # Create folder if needed
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self) -> bool:
+        if self.n_calls % self.check_freq == 0:
+
+          # Retrieve training reward
+          x, y = ts2xy(load_results(self.log_dir), 'timesteps')
+          if len(x) > 0:
+              # Mean training reward over the last 100 episodes
+              mean_reward = np.mean(y[-100:])
+              if self.verbose > 0:
+                print("Num timesteps: {}".format(self.num_timesteps))
+                print("Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(self.best_mean_reward, mean_reward))
+
+              # New best model, you could save the agent here
+              if mean_reward > self.best_mean_reward:
+                  self.best_mean_reward = mean_reward
+                  # Example for saving best model
+                  if self.verbose > 0:
+                    print("Saving new best model to {}".format(self.save_path))
+                  self.model.save(self.save_path)
+
+        return True
+
 env = gym.make(env_id)
-env = DummyVecEnv([lambda: env])
-model = PPO2(CnnLstmPolicy, env, verbose=1, nminibatches=1, tensorboard_log="D:/Hmm/tensorboard", learning_rate=0.0005, gamma=0.9997, n_steps=128, noptepochs=4, cliprange_vf=0.3, n_cpu_tf_sess=3)
-#model.load("best_model")
-model.learn(total_timesteps=1500000)
-model.save("agent")
+env = DummyVecEnv([lambda: env for _ in range(1)])
+env = Monitor(env, log_dir)
+model = PPO2(CnnLstmPolicy, env, verbose=1, gamma=0.95, nminibatches=1, noptepochs=4, n_steps=128, cliprange=0.1, learning_rate=0.00075, tensorboard_log="D:/Hmm/tensorboard"   )
+callback = SaveOnBestTrainingRewardCallback(check_freq=1500, log_dir=log_dir)
+model.learn(total_timesteps=10000000, callback=callback)
 # Enjoy trained agent
 obs = env.reset()
-state = Non
+state = None
 while True:
     action, state = model.predict(obs, state=state)
     obs, rewards, dones, info = env.step(action)
